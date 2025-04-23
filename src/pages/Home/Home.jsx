4f586736fef9e3f879196f82afbe464b45293cfa
Heryson Andrade <141951314+ribshow@users.noreply.github.com>
2025-04-23 09:29:47 -0300

    class(23/04)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,57 @@
 import styles from './Home.module.css';
+import api from "../../utils/api";
+import { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
+    const [pets, setPets] = useState([]);
+    const token = localStorage.getItem("token");
+
+    useEffect(() => {
+        api.get("/pets/getAll", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            console.log(response);
+            setPets(response.data);
+        });
+    }, [])
     return (
-        <div className={styles.home}>
-            <h1>Hello world</h1>
-        </div>
+        <section>
+            <div className={styles.home}>
+                <h1>Bem-vindo ao meu petsumiu</h1>
+                <h3>Encontre seu melhor amigo!</h3>
+            </div>
+            <div className={styles.pet_container}>
+                {pets && (
+                    pets.map((pet) => (
+                        <div className={styles.pet_card} key={pet._id}>
+                            <div className={styles.pet_card_header}>{pet.situation}</div>
+                            <div className={styles.pet_card_image}
+                                style={{
+                                    backgroundImage:
+                                        `url(${process.env.REACT_APP_API}/images/pets/${pet.images?.[0] || "default.jpg"})`
+                                }}>
+
+                            </div>
+                            <div className={styles.pet_card_body}>
+                                <h3>{pet.name || "Nome não informado :("}</h3>
+                                <Link to={`/pet/${pet._id}`} className={styles.details_button}>
+                                    Ver mais
+                                </Link>
+                            </div>
+                        </div>
+                    ))
+                )}
+                {!pets && (
+                    <p>Não há pets cadastrados no momento :D</p>
+                )}
+            </div>
+        </section>
+
     )
 }
 
